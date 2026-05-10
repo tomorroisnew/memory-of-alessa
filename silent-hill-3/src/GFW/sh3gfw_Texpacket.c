e2773545840850b6803c5df1406d9b2174311177
moth dreams <mothdreams666@gmail.com>
2026-05-09 21:23:08 -0700

    sh3: split `sh3gfw_Texpacket`

---
+++ silent-hill-3/src/GFW/sh3gfw_Texpacket.c
@@ -0,0 +1,47 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B5030);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", sh3gfw_set_TexToTrasMan);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", sh3gfw_get_newMANID);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", sh3gfw_del_TexMAN);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B56F0);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", sh3gfw_init_SyncTexTag);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B5AC0);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B5AD0);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B5AE0);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B5AF0);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B5B00);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B5B80);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", sh3gfw_Thr_d2TextureSend);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", sh3gfw_Thr_d1d2SyncKick);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", Fake_SetTexSlot);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", sh3gfw_Lock_AllTexSlot_For_Shadow);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", sh3gfw_UnLock_AllTexSlot_For_Shadow);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B5DA0);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", sh3gfw_SetSlot2Tex);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B6070);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B60B0);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B60F0);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_Texpacket", func_001B6240);
